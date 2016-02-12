7c57d6d01caf53e09f20dcf7d63fc6b4a12e7ebb
sp0ck <ernstlich.heiter@gmail.com>
2016-02-12 16:26:39 +0100

    fixed pokerth logo firefox issue, added contributions

--- template/default/html/core/foot/foot.html.php
+++ template/default/html/core/foot/foot.html.php
@@ -9,16 +9,41 @@
     <hr />
   </div>
 </div>
-<div class="row footer">
-  <div class="col-md-8 col-md-offset-2 text-center">
-    <h4 class="text-success">Thanks to all players who help as game admin:</h4>
-    <p>
-      Garline, sp0ck, Pagol, hargreaves, ganja, Saufi,<br />
-      israelac, 827, Scotty, FradiFan &amp; G4Zi<br />
-      Dati, Nelly, GAVIOES
-    </p>
+<div class="footer">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+      <h4 class="text-success">Thanks to all players who help as game admin:</h4>
+      <p>
+        Garline, sp0ck, Pagol, hargreaves, ganja, Saufi,<br />
+        israelac, 827, Scotty, FradiFan &amp; G4Zi<br />
+        Dati, Nelly, GAVIOES
+      </p>
+    </div>
   </div>
-  <div class="col-md-2 pull-right">
-    <a href="http://pokerth.net" title="PokerTH" target="_blank"><img src="/res/pic/pth_logo.png" alt="PokerTH" /></a>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+      <br />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+      <a href="http://pokerth.net" title="PokerTH" target="_blank"><img src="/res/pic/pth_logo.png" alt="PokerTH" /></a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+      <br />
+      <br />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+      <p>
+        This site was built using <a href="http://jquery.com">jQuery</a>,
+        <a href="http://getbootstrap.com/">Bootstrap</a>,
+        <a href="http://malsup.com/jquery/form/">jQuery Form Plugin</a>,
+        <a href="http://plugins.krajee.com/file-input">bootstrap-fileinput</a>
+      </p>
+    </div>
   </div>
 </div>
